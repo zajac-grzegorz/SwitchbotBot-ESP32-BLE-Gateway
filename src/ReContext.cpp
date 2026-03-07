9434fecd618351fce1e1f99854c331e99b8692ae
Grzegorz Zajac <grzegorz.zajac@outlook.com>
2026-03-07 18:53:15 +0100

    Introduce ReContext class to manage context information.

---
+++ src/ReContext.cpp
@@ -0,0 +1,13 @@
+#include "ReContext.h"
+
+PsychicMqttClient ReContext::mqttClient;
+AsyncWebServer* ReContext::server = nullptr;
+AsyncWebServerRequestPtr ReContext::pressRequest;
+AsyncAuthenticationMiddleware ReContext::basicAuth;
+Mycila::ESPConnect* ReContext::espConnect = nullptr;
+const NimBLEAdvertisedDevice* ReContext::advDevice = nullptr;
+NimBLEScan* ReContext::pScan = nullptr;
+bool ReContext::doConnect = false;
+std::string ReContext::doCommand = "570100"; // default is press command
+MatterOnOffPlugin ReContext::OnOffPlugin;
+
