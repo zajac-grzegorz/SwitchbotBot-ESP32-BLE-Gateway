279650adaa0fe2d3be45af87f8ff9ad1e5afad0e
Grzegorz Zajac <grzegorz.zajac@outlook.com>
2026-03-11 21:39:38 +0100

    First changes to refactor the code structure and make it more modular.

--- src/ReContext.cpp
+++ src/ReContext.cpp
@@ -1,12 +1,12 @@
 #include "ReContext.h"
 
 PsychicMqttClient ReContext::mqttClient;
-AsyncWebServer* ReContext::server = nullptr;
-AsyncWebServerRequestPtr ReContext::pressRequest;
-AsyncAuthenticationMiddleware ReContext::basicAuth;
-Mycila::ESPConnect* ReContext::espConnect = nullptr;
-const NimBLEAdvertisedDevice* ReContext::advDevice = nullptr;
-NimBLEScan* ReContext::pScan = nullptr;
+// const AsyncWebServer* ReContext::server = nullptr;
+// AsyncWebServerRequestPtr ReContext::pressRequest;
+// const AsyncAuthenticationMiddleware ReContext::basicAuth;
+// const Mycila::ESPConnect* ReContext::espConnect = nullptr;
+// const NimBLEAdvertisedDevice* ReContext::advDevice = nullptr;
+// const NimBLEScan* ReContext::pScan = nullptr;
 bool ReContext::doConnect = false;
 std::string ReContext::doCommand = "570100"; // default is press command
 MatterOnOffPlugin ReContext::OnOffPlugin;
